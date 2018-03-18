851f0441b0ab3dad09edba4b548955a9d5b3ce89
mattperkins <github@mattperkins.com>
2018-03-18 01:38:12 +0000

    restripe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {Component, CreateContext} from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
 import styled, {injectGlobal} from 'styled-components'
@@ -16,14 +16,6 @@ const Wrapper = styled.div`
 margin: 100px;
 `
 
-const AppContext = createContext()
-
-const { Provider, Consumer } = AppContext
-
-const initialState = {
-  users: [ 'Fred', 'Sandy', 'Bob' ]
-}
-
 
 // main ("ROOT") component 
 export default class Root extends Component {
@@ -33,11 +25,9 @@ render() {
 
 // MAIN COMPONENT RETURN
 return (
- <Wrapper>
-
-Hello
- </Wrapper>
-
+  <Wrapper>
+   Hello
+  </Wrapper>
 )// end return
 }// end render
 }// end component
