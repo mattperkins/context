0e41cd0a7a73e0715796a2519a4be86158394c79
mattperkins <github@mattperkins.com>
2018-03-18 00:22:20 +0000

    init

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+import styled, {injectGlobal} from 'styled-components'
+import Person from './Person'
+
+//eslint-disable-next-line
+injectGlobal`
+body {
+font-family: sans-serif;
+background: #f7f7f7;
+}
+`
+
+// styled components
+const Wrapper = styled.div`
+margin: 100px;
+`
+
+
+// main ("ROOT") component 
+export default class Root extends Component {
+state={
+ name: 'Fred',
+ age: 34,
+ city: 'LA'
+}
+
+// MAIN COMPONENT RENDER
+render() {
+
+// MAIN COMPONENT RETURN
+return (
+
+<Wrapper>
+
+
+<Person name={this.state.name}/>
+
+</Wrapper>
+
+)// end return
+}// end render
+}// end component
+
+ReactDOM.render(<Root />, 
+  document.getElementById('root'))
+  registerServiceWorker()
